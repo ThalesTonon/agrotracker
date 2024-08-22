e0ddae5b5a10a19094be42bcb74c78c478522a98
Thales Tonon <tcttonon@gmail.com>
2024-08-22 13:19:24 -0300

    Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,3 @@
-import { useAxiosGet } from "@/api/useAxios";
-
-type User = {
-  id: number;
-  title: string;
-  description: string;
-  start: string;
-  end: string;
-  user_id: number;
-  created_at: string;
-  updated_at: string;
-};
-
 export default function Home() {
   return (
     <div>
